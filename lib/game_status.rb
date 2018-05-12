45e4ee58361b51271cfd29b5104dc206cd2fd1fa
derrick b <dbozich1@gmail.com>
2018-05-12 19:09:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,11 +31,11 @@ def full?(board)
 end
 
 def draw?(board)
- full?(board) && !won(board) || !full?(board) && won?(board)
+ full?(board) || !won(board) 
 end
 
 def over?(board)
-  won(board) || full(board)
+  won?(board) || full?(board)
 end
 
 def winner?(board)
